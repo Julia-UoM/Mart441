e801b374ddb3a8529e0bb8f9dcad7f8ffe98c164
Julia-UoM <jp225661@umconnect.umt.edu>
2026-01-30 16:45:13 -0700

    Update script.js

--- HW3/script.js
+++ HW3/script.js
@@ -3,12 +3,28 @@ Author: Julia
 Date: Jan 22, 2026
 Purpose: Practice using comments and console.log
 */
+// -------------------------------
+// Console Logging Homework
+// -------------------------------
 
-// Print a greeting
-console.log("Hello, world!");
+// Three of my favorite websites
+console.log("Favorite Websites:");
+console.log("1. https://www.therealreal.com - Second-hand designer shopping");
+console.log("2. https://www.clueperfumery.com - A perfume house with a super cool website");
+console.log("3. https://www.miumiu.com/us/en.html - Editorial + elegant web design");
 
-// Print a number
-console.log(42);
+// Three of my favorite games (digital or physical)
+console.log("\nFavorite Games:");
+console.log("1. Stardew Valley - Digial");
+console.log("2. Ghostwire Tokyo - Digial");
+console.log("3. Graveyard Keeper - Digital");
 
-// Print a sentence
-console.log("JavaScript is running!");
+// Three of my favorite artists and their cultural importance
+console.log("\nFavorite Artists:");
+console.log("1. Ai Weiwei - Multi-media artist known for using art and activism to confront statepower, censorship, and human rights abuses.");
+console.log("2. Ren Hang - Photographer known for creating photographs that challenged taboos, censorship, and state control in contemporary China.");
+console.log("3. Yayoi Kusama - Multi-media artist known for transforming personal experiences of obsession and mental illness into immersive, repetitive visual worlds that influenced contemporary art, feminism, and pop culture.");
+
+// Simple question-and-answer interaction
+let userAnswer = prompt("What's your favorite game?");
+console.log("Your favorite game is: " + userAnswer);
